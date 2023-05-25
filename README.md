46f4db92251fb7ed2e2c684f952cfcedc35035a2
DaBenjamins <mike@benja.cc>
2023-05-25 00:12:52 -0400

    Initial

--- README.md
+++ README.md
@@ -2,46 +2,54 @@
 This tool allows you to dynamically update content, color and visibility of text sources in OBS through a google sheet.
 
 # Prerequisites
-- Node.js (tested on [16.7](https://nodejs.org/download/release/v16.7.0/node-v16.7.0-x64.msi))
+- [Node.js (tested on 18.16)](https://nodejs.org/en/download)
 - [Google Sheets API Key](https://developers.google.com/sheets/api/guides/authorizing#APIKey)
-- [OBS >= 28.0](https://obsproject.com/)
+- [Google sheet (example)](https://docs.google.com/spreadsheets/d/1Z1MlruzHm0UYCNO4cTlcDtUdWrMaRB5gwkVy7Zthl8c/)
+- [OBS Asynchronous image source Plugin (recommended if updating image sources)](https://obsproject.com/forum/resources/xobsasyncimagesource-asynchronous-image-source.1681/)
 
 # Installation
 - Download the source
-- Open PowerShell/cmd in the source directory
-- Run `npm install`
+- Download and Install Node.js
+- Run install.bat
 
 # Usage
 ## Spreadsheet
 The source spreadsheet needs to be set to allow anyone with the link to view, otherwise the API connection doesn't work.
 
-## OBS Setup
-Make sure the Websocket is enabled (`Tools -> WebSocket Server Settings -> Enable WebSocket Server`), take not of the settings (Port and Password)
-
 ## Configuration
 Open `config-dist.json` and set the following values:
 - sheetId: The ID of the google sheet (you can copy this from the sheets link, e.g. `https://docs.google.com/spreadsheets/d/this-bit-here-is-the-sheet-id/edit#gid=0`)
 - tabname: The tab name where you need data from
-- range: The range of cells you need data from (in A1 notation, e.g. `D2:G7`)
+- range: The range of Rows you need data from, must be column C through D (in A1 notation, e.g. `C1:D50`)
 - apikey: Your Google Sheets API Key
-- obsaddress: The address of your OBS Websocket, default is localhost:4444 for local OBS instance
+- obsaddress: The address of your OBS Websocket, default is ws://localhost:4455 for local OBS instance
 - obsauth: The password configured in your OBS Websocket, leave empty for no authentication
 - polling: The frequency of updates in ms, defaults to 2000 (the google API maxes out at 1 request per second)
 - dimension: controls the major dimension in the API query, don't touch unless you know what you are doing
 
 Save the file as `config.json`
 
-## OBS Scene
-To enable an OBS source to be controlled via the sheet, name the source in this format: `[Arbitrary Source Name] |sheet [Cell]`
+## OBS Setup
+To enable an OBS source to be controlled via the sheet, name the source in this format: `[Arbitrary Source Name] |sheet [Row #]`
 
-For example, `Text Source 1 |sheet C5` would be controlled by the contents of cell C5.
-
-**The tool does not work with groups. Using groups is not recommended in general, use nested scenes instead**
+For example, `Text Source 1 |sheet 5` would be controlled by the contents of Row 5.
 
 ## Controlling Text Sources
 ### Setting Text
 Any text in a cell is set as the text in the source. Empty cells are not updated.
-### Hiding/Showing a source
-Change the cell text to begin with `?hide;Any text here` or `?show; Any text here` to disable/enable the text source (with the last text that was set)
-### Changing text color
+### Hiding/Showing a Text Source
+Change the cell text to begin with `?hide;Any text here` or `?show;Any text here` to disable/enable the text source (with the last text that was set)
+### Changing Text Color
 Change the cell text to `?color=000000;Any text here` to set the text color in OBS with hex colors.
+
+## Controlling Image Sources
+Any image URL in a cell is set as the image in the source. Empty cells are not updated.
+
+## Controlling Color Sources
+Any #hexcode in a cell is set as the color in the source. Empty cells are not updated.
+
+## Controlling Browser Sources
+Any URL in a cell is set as the webpage in the source. Empty cells are not updated.
+
+## Controlling Visability Only
+Any "hide"/"show" in a cell is set as the sources visability. Empty cells are not updated.
--- index.js
+++ index.js
@@ -3,91 +3,201 @@ const {default: OBSWebSocket} = require('obs-websocket-js');
 const sheetLoader = require('./sheet-loader');
 const config = require('./config.json');
 
-const getChildren = sources => {
-  let items = sources;
-  sources.forEach(source => {
-    if (source.type === 'group') {
-      items = items.concat(getChildren(source.groupChildren));
-    }
-  });
-  return items;
-}
-
 const update = async (obs) => {
-  const data = await sheetLoader.loadData();
+	const data = await sheetLoader.loadData();
+  
+  	const { readFileSync } = await require('fs');
+	json = readFileSync('./data.json', 'utf8');
+	if (json == undefined){
+		json = [];
+	}
+	json = JSON.parse(json);
+	
+		//check if sheets is same as json
+	if ( data.toString() != json.toString()) {
+		
+		console.log("Sheets Updated");		
+		
+		// Write data.json to check if sheets been changed
+		const fs = await require('fs');
+		const jsonContent = await JSON.stringify(data);
+		await fs.writeFile("./data.json", jsonContent, 'utf8', function (err) {
+			if (err) {
+				return console.log(err);
+			}
+			console.log("The file was saved!");
+		});
+    
+		const range = config.range;
+		const startcell = range.split(":")[0].trim();
 
-  const range = config.range;
-  const startcell = range.split(":")[0].trim();
+		const startrow = startcell.match("[0-9]+");
 
-  const startcol = startcell.match("[a-zA-Z]+");
-  const startrow = startcell.match("[0-9]+");
+		const rowoffset = startrow[0];
 
-  const rowoffset = startrow[0];
-  const coloffset = columnToNumber(startcol[0]);
+		const sceneList = await obs.call('GetSceneList');
+		await sceneList.scenes.forEach(async scene => {
+			// unfold group children
+			const allSources = await obs.call('GetSceneItemList', {sceneName: scene.sceneName});
+			//const allSources = getChildren(allSourcesNGroups);
+			await allSources.sceneItems.forEach(async source => {
+			if (source.sourceName.includes('|sheet')) {
+				const reference = source.sourceName.split('|sheet')[1].trim();
+			
+				let row = reference.match("[0-9]+");
+				let rownumber = row[0] - rowoffset;
 
-  const sceneList = await obs.call('GetSceneList');
-  await sceneList.scenes.forEach(async scene => {
-    // unfold group children
-    const allSources = await obs.call('GetSceneItemList', {sceneName: scene.sceneName});
-    await allSources.sceneItems.forEach(async source => {
-      if (source.sourceName.includes('|sheet') && source.inputKind.includes('gdiplus')) {
-        const reference = source.sourceName.split('|sheet')[1].trim();
-
-        let col = reference.match("[a-zA-Z]+");
-        let colnumber = columnToNumber(col[0]) - coloffset;
-        
-        let row = reference.match("[0-9]+");
-        let rownumber = row[0] - rowoffset;
-
-        let cellvalue = data[colnumber][rownumber];
-
-            if (cellvalue !== 'undefined') {
-              let color = null;
-
-              if (cellvalue.startsWith('?color')) {
-                const split = cellvalue.split(';');
-                cellvalue = split[1];
-                color = split[0].split('=')[1];
-                color = color.replace('#', '');
-                const color1 = color.substring(0, 2);
-                const color2 = color.substring(2, 4);
-                const color3 = color.substring(4, 6);
-                color = parseInt('ff' + color3 + color2 + color1, 16);
-              }
-
-              if (cellvalue.startsWith('?hide')) {
-                const split = cellvalue.split(';');
-                cellvalue = split[1];
-                await obs.call("SetSceneItemEnabled", {
-                  sceneName: scene.sceneName,
-                  sceneItemId: source.sceneItemId,
-                  sceneItemEnabled: false
-                });
-              } else if (cellvalue.startsWith('?show')) {
-                const split = cellvalue.split(';');
-                cellvalue = split[1];
-                await obs.call("SetSceneItemEnabled", {
-                  sceneName: scene.sceneName,
-                  sceneItemId: source.sceneItemId,
-                  sceneItemEnabled: true
-                });
-              }
-              
-              // Update to OBS
-              await obs.call("SetInputSettings", {
-                inputName: source.sourceName,
-                inputSettings: {
-                  text: cellvalue,
-                  color: color
-                }
-              });
-              console.log(`Updated: ${reference} to OBS: ${source.sourceName}`);
-            } else {
-              console.log(`Field ${reference} is empty`)
-        }
-      }
-    });  
-  });
+				let cellvalue = data[1][rownumber];
+				let sourcetype = data[0][rownumber];
+			
+					// If Cell is empty skip
+					if (cellvalue != undefined) {
+						// If Source type is Text
+						if (sourcetype == "Text"){
+							let color = null;
+							//check if ?color tag is present
+							if (cellvalue.startsWith('?color')) {
+								const split = cellvalue.split(';');
+								cellvalue = split[1];
+								color = split[0].split('=')[1];
+								color = color.replace('#', '');
+								const color1 = color.substring(0, 2);
+								const color2 = color.substring(2, 4);
+								const color3 = color.substring(4, 6);
+								color = parseInt('ff' + color3 + color2 + color1, 16);
+							}
+							//check if ?hide/?show tag is present
+							if (cellvalue.startsWith('?hide')) {
+								const split = cellvalue.split(';');
+								cellvalue = split[1];
+								await obs.call("SetSceneItemEnabled", {
+									sceneName: scene.sceneName,
+									sceneItemId: source.sceneItemId,
+									sceneItemEnabled: false
+								});
+							} else if (cellvalue.startsWith('?show')) {
+								const split = cellvalue.split(';');
+								cellvalue = split[1];
+								await obs.call("SetSceneItemEnabled", {
+									sceneName: scene.sceneName,
+									sceneItemId: source.sceneItemId,
+									sceneItemEnabled: true
+								});
+							}
+							//get settings of source from OBS
+							let textsettings = await obs.call("GetInputSettings", {
+								inputName: source.sourceName
+							});
+							let oldfile = await textsettings['inputSettings']['text']
+							let oldcolor = await textsettings['inputSettings']['color']
+							//check if current OBS settings is different
+							if (cellvalue != oldfile){
+								if (color == null){
+									color = oldcolor
+								}
+								// Update to OBS
+								await obs.call("SetInputSettings", {
+									inputName: source.sourceName,
+									inputSettings: {
+										text: cellvalue,
+										color: color
+									}
+								});
+								console.log(`Updated: ${reference} to OBS: ${source.sourceName}`);
+							} else {
+								console.log('text is the same');
+							}
+							
+						}
+						// If Source type is Color
+						if (sourcetype == "Color"){
+							let color = null;
+							color = cellvalue
+							color = color.replace('#', '');
+							const color1 = color.substring(0, 2);
+							const color2 = color.substring(2, 4);
+							const color3 = color.substring(4, 6);
+							color = parseInt('ff' + color3 + color2 + color1, 16);
+							//get settings of source from OBS
+							let colorsettings = await obs.call("GetInputSettings", {
+								inputName: source.sourceName,
+							});
+							let oldfile = await colorsettings['inputSettings']['color']
+							//check if current OBS settings is different
+							if (color != oldfile){
+								console.log(`Updated: ${reference} to OBS: ${source.sourceName}`);
+								await obs.call("SetInputSettings", {
+									inputName: source.sourceName,
+									inputSettings: {
+										color: color
+									}
+								});	
+							} else {
+								console.log('Color is the same');
+							}
+						}
+						// If Source type is Image
+						if (sourcetype == "Image"){
+							//get settings of source from OBS
+							let imagesettings = await obs.call("GetInputSettings", {
+								inputName: source.sourceName,
+							});	
+							let oldfile = await imagesettings['inputSettings']['file']
+							//check if current OBS settings is different
+							if (cellvalue != oldfile){
+								console.log(`Updated: ${reference} to OBS: ${source.sourceName}`);
+								await obs.call("SetInputSettings", {
+									inputName: source.sourceName,
+									inputSettings: {
+										file: cellvalue
+									}
+								});	
+							} else {
+								console.log('Image is the same');
+							}
+						}
+						// If Source type is Browser
+						if (sourcetype == "Browser"){
+							//get settings of source from OBS
+							let browsersettings = await obs.call("GetInputSettings", {
+								inputName: source.sourceName
+							});
+							let oldfile = await browsersettings['inputSettings']['url']
+							//check if current OBS settings is different
+							if (cellvalue != oldfile){
+								console.log(`Updated: ${reference} to OBS: ${source.sourceName}`);
+								await obs.call("SetInputSettings", {
+									inputName: source.sourceName,
+									inputSettings: {
+										url: cellvalue
+									}
+								});
+							} else {
+								console.log('Browser is the same');
+							}
+						}
+						// If Source type is HS
+						if (sourcetype == "HS"){
+							if (cellvalue.startsWith('hide')) {
+								await obs.call("SetSceneItemEnabled", {
+									sceneName: scene.sceneName,
+									sceneItemId: source.sceneItemId,
+									sceneItemEnabled: false
+								});
+							} else if (cellvalue.startsWith('show')) {
+								await obs.call("SetSceneItemEnabled", {
+									sceneName: scene.sceneName,
+									sceneItemId: source.sceneItemId,
+									sceneItemEnabled: true
+								});
+							}
+							console.log(`Updated: ${reference} to OBS: ${source.sourceName}`);
+						}
+					}
+				}
+			});  
+		});
+	}
 }
 
 const main = async () => {
